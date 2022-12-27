6d55636a8e7bdc0eeb5e9bbb4a6f32b93441233b
Rahul Roy <raulrahulroy@gmail.com>
2022-12-27 20:32:01 +0530

    udf fix

--- kitemoney/test/test_main.py
+++ kitemoney/test/test_main.py
@@ -1,11 +1,10 @@
+#%%
 import yaml
 import os
 
-with open('kitemoney/Config/config.yaml', 'r') as confyaml :
-    conf = yaml.load(confyaml, Loader=yaml.FullLoader)
 
-assert 'Path' in conf[0].keys(), 'Path Not Set'
-assert 'cred' in conf[0]['Path'].keys(), 'Path Not Set'
+#%%
+# UDF
 
 def get_cred(conf) :
     cred_path_wo_sep_list = conf[0]['Path']['cred'].split('/')
@@ -17,9 +16,33 @@ def get_cred(conf) :
             cwd_sep_wo_sep_list.append(dir)
 
     cred_path = os.sep.join(cwd_sep_wo_sep_list)
+    try :
+        with open(cred_path, 'r') as credyaml :
+            cred = yaml.load(credyaml, Loader=yaml.FullLoader)
+    except FileNotFoundError as e :
+        #print ('check relative file path in Config.config.yaml')
+        raise Exception(e, 'check relative file path in Config.config.yaml')
+    assert 'Credentials' in cred[0].keys(), 'Credentials Not Set'
+    assert 'USER_ID' in cred[0]['Credentials'].keys(), 'USER_ID Not Set'
+    USER_ID = cred[0]['Credentials']['USER_ID']
+    assert 'PASSWD' in cred[0]['Credentials'].keys(), 'PASSWD Not Set'
+    PASSWD = cred[0]['Credentials']['PASSWD']
+    assert 'TOKEN' in cred[0]['Credentials'].keys(), 'TOKEN Not Set'
+    TOKEN = cred[0]['Credentials']['TOKEN']
 
-    with open(cred_path, 'r') as credyaml :
-        cred = yaml.load(credyaml, Loader=yaml.FullLoader)
-    print (cred)
+    return (USER_ID, PASSWD, TOKEN)
 
-get_cred(conf)
+#%%
+with open('Config/config.yaml', 'r') as confyaml :
+    conf = yaml.load(confyaml, Loader=yaml.FullLoader)
+
+assert 'Path' in conf[0].keys(), 'Path Not Set'
+assert 'cred' in conf[0]['Path'].keys(), 'Path Not Set'
+
+#%%
+USER_ID, PASSWD, TOKEN, = get_cred(conf)
+
+print (USER_ID, PASSWD, TOKEN)
+
+# %%
+
